ca57861dbc9cdf686e14ca6437a6a5083212af39
HaydenSD <haydensd@protonmail.ch>
2016-12-22 12:04:46 -0500

    Added first binds

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).keydown(function(e){
+  if(e.which == 68) {
+    window.location.replace("https://democracynow.org");
+  }
+  if(e.which == 73) {
+    window.location.replace("https://theintercept.com");
+  }
+  if(e.which == 80) {
+    window.location.replace("https://pandora.com");
+  }
+})
