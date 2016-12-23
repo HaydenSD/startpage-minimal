55f474d5e88a96117346827c6aefad3db7ff9afa
HaydenSD <haydensd@protonmail.ch>
2016-12-23 09:46:51 -0500

    Added remaining keybinds

--- js/main.js
+++ js/main.js
@@ -8,4 +8,19 @@ $(document).keydown(function(e){
   if(e.which == 80) {
     window.location.replace("https://pandora.com");
   }
+  if(e.which == 82) {
+    window.location.replace("https://reddit.com/me/m/master_race");
+  }
+  if(e.which == 78) {
+    window.location.replace("https://nixers.net");
+  }
+  if(e.which == 89) {
+    window.location.replace("https://youtube.com");
+  }
+  if(e.which == 67) {
+    window.location.replace("https://classroom.google.com");
+  }
+  if(e.which == 84) {
+    window.location.replace("https://twitter.com")
+  }
 })
